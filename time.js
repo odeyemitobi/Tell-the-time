5ebc5452dee0110956444f4e42968bcd6b4763e7
odeyemitobi <105063166+odeyemitobi@users.noreply.github.com>
2022-10-04 13:14:31 +0100

    done

--- time.js
+++ time.js
@@ -38,4 +38,32 @@ function showTime(){
 
     document.getElementById("DisplayClock").innerHTML = time;
     setTimeout(showTime,1000)
+
+
+    //  Change background
+
+    let bg;
+
+    const userName = "IT'S CURRENTLY";
+    const user = document.getElementById("User");
+
+    // console.log(user);
+    if (h < 8 && session === "AM") {
+        bg = `url(https://source.unsplash.com/G_a9JhmiZFk)`;
+        user.innerHTML = `GOOD MORNING ${userName}`; 
+    } else if (h < 11 && session === "AM") {
+        bg =  `url(https://source.unsplash.com/XJnG9OsmS2c)`;
+        user.innerHTML = `GOOD DAY ${userName}`;
+    } else if (h < 8 && session === "PM") {
+        bg =  `url(https://source.unsplash.com/sKXLdjfIv9w)`;
+        user.innerHTML = `GOOD AFTERNOON, ${userName}`;
+    } else {
+        bg =  `url(https://source.unsplash.com/toX2sYnycCw)`;
+        user.innerHTML = `GOOD NIGHT, ${userName}`;
+    }
+
+    // insert bg img
+    const body=document.querySelector("body");
+
+    body.style.background =`${bg} center/cover`;
 }
